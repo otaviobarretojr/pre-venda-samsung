262267d7c8b5a30461d16034288b613dcd99d217
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-13 15:48:46 -0400

    chore: remove obsolete admin lock patch

--- admin-lock.js
+++
@@ -1,6 +0,0 @@
-// PRE VENDA v4.4.0
-// Arquivo legado mantido apenas para neutralizar Service Workers antigos.
-// Toda a lógica administrativa agora está integrada diretamente no index.html.
-(function(){
-  console.info('PRE VENDA v4.4.0: módulo legado neutralizado.');
-})();
