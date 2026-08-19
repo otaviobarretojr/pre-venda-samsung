8c3b075ac858d243362173ef5ab77ca865903cce
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-19 09:21:23 -0400

    release(v7): isolate RC1 cache and include release runtime

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pre-venda-samsung-v6-5-9';
+const CACHE_NAME = 'pre-venda-samsung-v7-0-0-rc1';
 const APP_SHELL = [
   './',
   './index.html',
@@ -27,6 +27,7 @@ const APP_SHELL = [
   './professional-v642-finalization.js',
   './enterprise-v650.js',
   './ecosystem-offers-v657.js',
+  './v7-release.js',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png'
