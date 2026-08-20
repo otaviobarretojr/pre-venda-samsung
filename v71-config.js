7de50b4d17afd7300470a35a13cdac08f04fc844
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-20 19:33:08 -0400

    release(v7.1): set stable runtime config

--- v71-config.js
+++ v71-config.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 const config=Object.freeze({
- version:'7.1.0-rc.1',
+ version:'7.1.0',
  stableBase:'7.0.2',
  data:{
   preSale:{table:'pre_vendas',localKey:'samsung_pre_vendas'},
